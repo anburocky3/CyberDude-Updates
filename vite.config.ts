9d5a03254c9fa6637fad8a1676ecf5a4eccac954
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-08-21 21:41:13 +0530

    feat: pwa integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import path from 'path';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  injectRegister: 'auto',
+  includeAssets: ['favicon.ico'],
+  manifest: {
+    name: 'CyberDude Updates',
+    short_name: 'CDUpdates',
+    description: 'Track CyberDude Updates.',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(pwaOptions)],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
